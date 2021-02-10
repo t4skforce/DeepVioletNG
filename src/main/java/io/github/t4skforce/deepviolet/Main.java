2b7f94dec887a6a505c58d5751a7a26d7486c6f7
t4skforce <t4skforce1337@gmail.com>
2021-02-10 23:40:09 +0100

    inital setup

---
+++ src/main/java/io/github/t4skforce/deepviolet/Main.java
@@ -0,0 +1,17 @@
+package io.github.t4skforce.deepviolet;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("=====================================================================");
+		System.out.println("=====================================================================");
+		System.out.print("main ( ");
+		for(String arg : args) {
+			System.out.print(arg+" ");
+		}
+		System.out.println(")");
+		System.out.println("=====================================================================");
+		System.out.println("=====================================================================");
+	}
+	
+}
