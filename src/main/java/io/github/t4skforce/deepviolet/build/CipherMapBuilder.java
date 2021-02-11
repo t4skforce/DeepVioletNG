19458c8ce5361b808a0b09bc90594410139c7991
t4skforce <t4skforce1337@gmail.com>
2021-02-11 15:31:24 +0100

    * refactoring, added messages

--- src/main/java/io/github/t4skforce/deepviolet/build/CipherMapBuilder.java
+++ src/main/java/io/github/t4skforce/deepviolet/build/CipherMapBuilder.java
@@ -2,19 +2,26 @@ package io.github.t4skforce.deepviolet.build;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.common.io.Resources;
 
 import io.github.t4skforce.deepviolet.json.CipherMapClassificationsJson;
 import io.github.t4skforce.deepviolet.json.CipherMapJson;
@@ -34,6 +41,9 @@ public class CipherMapBuilder {
 	private static String NSS_URL = "https://hg.mozilla.org/projects/nss/raw-file/tip/lib/ssl/sslproto.h";
 	private static String OPENSSL_URL = "https://raw.githubusercontent.com/openssl/openssl/master/include/openssl/tls1.h";
 	private static String GNUTLS_URL = "https://gitlab.com/gnutls/gnutls/raw/master/lib/algorithms/ciphersuites.c";
+	
+	private static final java.lang.String BASE_KEY = "cypher.builder";
+	private static final String CIPHERMAP_JSON = "ciphermap.json";
 
 	private static Pattern REGEX_IANA = Pattern.compile(
 			"<td[^>]*>(?<hex>0x[0-9|A-F]{2},0x[0-9|A-F]{2})</td[^>]*>[^<]*<td[^>]*>(?<name>TLS_[^\\s]+)</td[^>]*>",
@@ -54,81 +64,86 @@ public class CipherMapBuilder {
 			"#\\s*define\\s*GNU(?<name>TLS_[^\\s]+)\\s*\\{\\s*(?<hex1>0x[A-F|0-9]{2})\\s*,\\s*(?<hex2>0x[A-F|0-9]{2})\\s*\\}",
 			Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
 
-	private File target;
-
 	private CipherMapJson cipherMapJson = new CipherMapJson();
 
 	private Map<String, String> ianaNameMap = new HashMap<>();
 
-	private Consumer<String> logConsumer = m -> {};
+	private MessageConsumer<String,Object[]> logConsumer = (m,o) -> {
+	};
 
-	private Consumer<String> warnConsumer = m -> {};
+	private MessageConsumer<String,Object[]> warnConsumer = (m,o) -> {
+	};
+	
+	private ObjectMapper mapper;
+	
+	private static final ResourceBundle RES_BUNDLE = ResourceBundle.getBundle("Messages");
 
-	private Consumer<Exception> errorConsumer = e -> {};
-
-	private CipherMapBuilder(File target) {
-		this.target = target;
+	private CipherMapBuilder() {
+		mapper = new ObjectMapper();
+		mapper.configure(MapperFeature.USE_ANNOTATIONS, true);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 
-	public static CipherMapBuilder builder(File target) {
-		return new CipherMapBuilder(target);
+	public static CipherMapBuilder builder() {
+		return new CipherMapBuilder();
 	}
 
-	public CipherMapBuilder parse() throws Exception {
-		int cnt = 0;
-		log("Fetching IANA definition "+IANA_URL);
-		cnt = parseIANA();
-		log("found "+cnt+" entries");
-		log("Fetching NSS definition "+NSS_URL);
+	public CipherMapBuilder build() throws Exception {
+		parseIANA();
 		parseNSS();
-		log("found "+cnt+" entries");
-		log("Fetching OpenSSL definition "+NSS_URL);
 		parseOpenSSL();
-		log("found "+cnt+" entries");
-		log("Fetching GnuTLS definition "+NSS_URL);
 		parseGnuTLS();
-		log("found "+cnt+" entries");
 		return this;
 	}
 
-	public CipherMapBuilder write() throws JsonGenerationException, JsonMappingException, IOException {
+	public CipherMapBuilder write(File target) throws JsonGenerationException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(MapperFeature.USE_ANNOTATIONS, true);
 		ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
 		writer.writeValue(target, cipherMapJson);
 		return this;
 	}
+	
+	public CipherMapJson get() throws IOException {
+		if(MapUtils.isEmpty(cipherMapJson)) {
+			cipherMapJson = get(CIPHERMAP_JSON);
+		}
+		return cipherMapJson;
+	}
+	
+	public CipherMapJson get(File file) throws IOException {
+		cipherMapJson = mapper.readValue(file, CipherMapJson.class);
+		return cipherMapJson;
+	}
 
-	public CipherMapBuilder log(Consumer<String> logConsumer) {
+	public CipherMapJson get(String resourceName) throws IOException {
+		String json = Resources.toString(Resources.getResource(resourceName), StandardCharsets.UTF_8);
+		cipherMapJson = mapper.readValue(json, CipherMapJson.class);
+		return cipherMapJson;
+	}
+
+	public CipherMapBuilder log(MessageConsumer<String,Object[]> logConsumer) {
 		this.logConsumer = logConsumer;
 		return this;
 	}
 
-	public CipherMapBuilder warn(Consumer<String> warnConsumer) {
+	public CipherMapBuilder warn(MessageConsumer<String,Object[]> warnConsumer) {
 		this.warnConsumer = warnConsumer;
 		return this;
 	}
-
-	public CipherMapBuilder error(Consumer<Exception> errorConsumer) {
-		this.errorConsumer = errorConsumer;
-		return this;
+	
+	private void log(String key, Object... params) {
+		this.logConsumer.accept(StringUtils.joinWith(".",BASE_KEY,key),params);
 	}
 
-	private void log(String message) {
-		this.logConsumer.accept(message);
-	}
-
-	private void warn(String message) {
-		this.warnConsumer.accept(message);
-	}
-
-	private void error(Exception message) {
-		this.errorConsumer.accept(message);
+	private void warn(String key, Object... params) {
+		this.warnConsumer.accept(StringUtils.joinWith(".",BASE_KEY,key),params);
 	}
 
 	private int parseIANA() throws Exception {
-		Matcher sources = REGEX_IANA.matcher(Downloader.get(IANA_URL));
 		int cnt = 0;
+		log("info.fetching","IANA", IANA_URL);
+		Matcher sources = REGEX_IANA.matcher(Downloader.get(IANA_URL));
 		while (sources.find()) {
 			String hex = sources.group("hex").trim().toUpperCase().replaceAll("X", "x");
 			String name = sources.group("name").trim().toUpperCase();
@@ -143,19 +158,20 @@ public class CipherMapBuilder {
 			ianaNameMap.put(name, hex);
 			cnt++;
 		}
-
+		log("info.found",cnt);
 		return cnt;
 	}
 
 	private int parseNSS() throws Exception {
 		int cnt = 0;
+		log("info.fetching","NSS", NSS_URL);
 		Matcher sources = REGEX_NSS.matcher(Downloader.get(NSS_URL));
 		while (sources.find()) {
 			String hex = sources.group("hex").trim().toUpperCase().replaceAll("X", "x");
 			hex = String.format("%s,0x%s", hex.substring(0, 4), hex.substring(4, 6));
 			String name = sources.group("name").trim().toUpperCase();
 			if (!cipherMapJson.containsKey(hex)) {
-				warn("NSS code point " + hex + " (" + name + ") not in IANA registry");
+				warn("warn.not.iana","NSS",hex,name);
 				continue;
 			}
 			CipherMapClassificationsJson cipher = cipherMapJson.get(hex);
@@ -163,12 +179,13 @@ public class CipherMapBuilder {
 
 			cnt++;
 		}
-
+		log("info.found",cnt);
 		return cnt;
 	}
 
 	private int parseOpenSSL() throws Exception {
 		int cnt = 0;
+		log("info.fetching","OpenSSL", NSS_URL);
 		String content = Downloader.get(OPENSSL_URL);
 
 		// mapping e.g., ECDHE_RSA_WITH_AES_128_GCM_SHA256 ->
@@ -189,44 +206,52 @@ public class CipherMapBuilder {
 			hex = String.format("0x%s,0x%s", hex.substring(6, 8), hex.substring(8, 10));
 
 			if (!cipherMapJson.containsKey(hex)) {
-				warn("OpenSSL code point " + hex + " (" + name + ") not in IANA registry");
+				warn("warn.not.iana","NSS",hex,name);
 				continue;
 			}
 			CipherMapClassificationsJson cipher = cipherMapJson.get(hex);
 			cipher.setOpenssl(name);
-
 			cnt++;
 		}
-
+		log("info.found",cnt);
 		return cnt;
 	}
 
 	private int parseGnuTLS() throws Exception {
 		int cnt = 0;
+		log("info.fetching","GnuTLS", NSS_URL);
 		Matcher sources = REGEX_GNUTLS_NAMES.matcher(Downloader.get(GNUTLS_URL));
 		while (sources.find()) {
 			String hex = String.format("%s,%s", sources.group("hex1").trim().toUpperCase().replaceAll("X", "x"),
 					sources.group("hex2").trim().toUpperCase().replaceAll("X", "x"));
 			String name = sources.group("name").trim().toUpperCase();
 			if (!cipherMapJson.containsKey(hex)) {
-				warn("GnuTLS code point " + hex + " (" + name + ") not in IANA registry");
+				warn("warn.not.iana","NSS",hex,name);
 				continue;
 			}
 			CipherMapClassificationsJson cipher = cipherMapJson.get(hex);
 			cipher.setGnutls(name);
 			cnt++;
 		}
-
+		log("info.found",cnt);
 		return cnt;
 	}
+	
+	private static String format(String key, Object[] params) {
+		return MessageFormat.format(RES_BUNDLE.getString(key).replaceAll("'", "''"), params);
+	}
 
+	@FunctionalInterface
+	public interface MessageConsumer<T, U> {
+	    public void accept(T key, U params);
+	}
+
+	@SuppressWarnings("all")
 	public static void main(String[] args) throws Exception {
-		CipherMapBuilder.builder(Paths.get(args[0],args[1]).toFile())
-		.log(m -> {System.out.println("INFO: "+m);})
-		.warn(m -> {System.out.println("WARN: "+m);})
-		.error(e -> {
-			System.out.println("ERROR: "+e.getMessage());
-			e.printStackTrace();
-		}).parse().write();
+		CipherMapBuilder.builder().log((key,params) -> {
+			System.out.println("[INFO] " + format(key,params));
+		}).warn((key,params) -> {
+			System.out.println("[WARNING] " + format(key,params));
+		}).build().write(Paths.get(args[0] , CIPHERMAP_JSON).toFile());
 	}
 }
